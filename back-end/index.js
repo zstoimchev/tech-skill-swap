3d79476608a4b001bd07c8e453dc973e7b959334
zstoimchev <zivkostoimcev1551@outlook.com>
2024-01-20 17:50:04 +0100

    changed port

--- back-end/index.js
+++ back-end/index.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8127
 
 app.use(cookieParser());
 
