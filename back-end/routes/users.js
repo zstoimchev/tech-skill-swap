23581d3ccc4eb1360d82766745197b60ec0ea9ee
zstoimchev <zivkostoimcev1551@outlook.com>
2024-07-09 00:04:11 +0200

    logging in functionality with jwt token, registration

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,78 +1,107 @@
 const express = require("express")
 const users = express.Router();
-const DB = require('../db/dbConn.js')
+const DB = require('../DB/dbConn.js')
+const jwt = require('jsonwebtoken');
 
-users.post('/login', async (req, res, next) => {
+users.get('/all', async (req, res, next) => {
     try {
-        console.log(req.body);
-        const username = req.body.username;
-        const password = req.body.password;
-        if (username && password) {
-            const queryResult = await DB.AuthUser(username)
-            if (queryResult.length > 0) {
-                if (password === queryResult[0].user_password) {
-                    req.session.user = queryResult
-                    req.session.logged_in = true
-                    res.statusCode = 200;
-                    res.json({ user: queryResult[0], status: { success: true, msg: "Logged in" } })
-                } else {
-                    res.statusCode = 200;
-                    res.json({ user: null, status: { success: false, msg: "Username or password incorrect" } })
-                    console.log("INCORRECT PASSWORD")
-                }
-            } else {
-                res.statusCode = 200;
-                res.send({ user: null, status: { success: false, msg: "Username not registsred" } })
-            }
-        }
-        else {
-            res.statusCode = 200;
-            res.send({ logged: false, user: null, status: { success: false, msg: "Input element missing" } })
-            console.log("Please enter Username and Password!")
-        }
-        res.end();
+        const q = await DB.allUsers()
+        return res.json(q)
     } catch (err) {
-        console.log(err)
-        res.sendStatus(500)
-        next()
-    }
-});
-
-users.get('/session', async (req, res, next) => {
-    try {
-        res.json(req.session)
-    } catch (error) {
-        res.sendStatus(500)
+        consolee.log(err)
     }
 })
 
-//Inserts a new user in our database id field are complete
-users.post('/register', async (req, res, next) => {
+users.post('/login', async (req, res, next) => {
     try {
-        const username = req.body.username
-        const password = req.body.password
-        const email = req.body.email
-        if (username && password && email) {
-            const queryResult = await DB.AddUser(username, email, password);
-            if (queryResult.affectedRows) {
-                res.statusCode = 200;
-                res.send({ status: { success: true, msg: "New user created" } })
-                console.log("New user added!!")
+        const tokenD = req.headers['authorization']
+        if (tokenD) {
+            try {
+                const tokenDecoded = jwt.verify(tokenD, 'i-love-programming');
+                return res.json({ success: false, token: tokenD, msg: "User is already logged in!" });
+            } catch (err) {
+                console.log(err)
             }
         }
-        else {
-            res.statusCode = 200;
-            res.send({ status: { success: false, msg: "Input element missing" } })
-            console.log("A field is missing!")
+
+        console.log(req.body)
+
+        const username = req.body.username
+        const password = req.body.password
+        if (!(username && password)) {
+            console.log("Please enter both Username & Password!")
+            res.json({ success: false, token: null, msg: "Please enter both Username & Password!" })
+            return
         }
-        res.end();
+
+        // TODO: validate input before sending to DB to prevent SQL injection
+        const queryResult = await DB.authUsername(username)
+        if (queryResult <= 0) {
+            console.log("Username does not exist. Please create new account!")
+            res.json({ success: false, token: null, msg: "Username does not exist. Please create new account!" })
+            return
+        }
+        if (password !== queryResult[0].password) {
+            console.log("Incorrect password!")
+            res.json({ success: false, token: null, msg: "Incorrect password!" })
+            return
+        }
+        const token = jwt.sign({ username: username }, 'i-love-programming'/*, { expiresIn: 'never' }*/);
+        res.json({ success: true, token: token, msg: "User is logged in!" })
     } catch (err) {
         console.log(err)
-        res.statusCode = 500;
-        res.send({ status: { success: false, msg: err } })
-        next()
     }
 
 });
+
+users.post('/register', async (req, res, next) => {
+    try {
+        // TODO: if user is already logged it, new user cannot be added, but for that sessions are needed
+
+        console.log(req.body)
+
+        const name = req.body.name
+        const surname = req.body.surname
+        const username = req.body.username
+        const email = req.body.email
+        const password = req.body.password
+        const password2 = req.body.password2
+
+        // TODO: check if email is really an email, consists of username [at] provider [dot] domain
+
+        // TODO: validate input before sending to DB to prevent SQL injection
+        const queryResultUsername = await DB.authUsername(username)
+        const queryResultEmail = await DB.authEmail(email)
+        console.log(queryResultEmail)
+
+        if (!(name && surname && username && email && password && password2)) {
+            return res.json({ success: false, msg: "Input field missing! Please fill in all the fields." })
+        }
+
+        if (queryResultEmail.length > 0) {
+            return res.json({ success: false, msg: "User with that E-mail already exists!" })
+        }
+
+        if (queryResultUsername.length > 0) {
+            return res.json({ success: false, msg: "User with that Username already exists!" })
+        }
+
+        if (password !== password2) {
+            return res.json({ success: false, msg: "Password does not match!" })
+        }
+
+        const queryResult = await DB.addUser(name, surname, username, email, password);
+        if (!(queryResult.affectedRows)) {
+            res.json({ success: false, msg: "Error registering new user..." })
+        }
+        return res.json({ success: true, msg: "New user succesfully registered." })
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+// TODO: session function, to check if there exists a session
+
+// TODO: logout funcrion, to log out already logged in user
 
 module.exports = users
