361f173f3afb94402383c2f1b22d534c11a04f21
zstoimchev <zivkostoimcev1551@outlook.com>
2024-07-11 23:37:10 +0200

    login/logout done, saving hashed password complete and working

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -34,95 +34,114 @@ async function comparePassword(userPassword, hashedPassword) {
 
 users.post('/login', async (req, res, next) => {
     try {
-        // const tokenD = req.headers['authorization']
-        // if (tokenD) {
-        //     try {
-        //         const tokenDecoded = jwt.verify(tokenD, 'i-love-programming');
-        //         return res.json({ success: false, token: tokenD, msg: "User is already logged in!" });
-        //     } catch (err) {
-        //         console.error(err)
-        //     }
-        // }
+        if (req.session.loggedIn) {
+            return res.status(200).json({ success: true, msg: "User is already logged in!" });
+        }
 
-        const username = req.body.username
-        const password = req.body.password
+        const { username, password } = req.body;
         if (!(username && password)) {
-            console.log("Please enter both Username & Password!")
-            res.json({success: false, token: null, msg: "Please enter both Username & Password!"})
-            return
+            return res.status(400).json({ success: false, msg: "Please enter both Username & Password!" });     // bad request
         }
 
         // TODO: validate input before sending to DB to prevent SQL injection
-        const queryResult = await DB.authUsername(username)
-        if (queryResult <= 0) {
-            console.log("Username does not exist. Please create new account!")
-            res.json({success: false, token: null, msg: "Username does not exist. Please create new account!"})
-            return
+        const queryResult = await DB.authUsername(username);
+        if (queryResult.length <= 0) {
+            return res.status(404).json({ success: false, msg: "Username does not exist. Please create new account!" });    // not found
         }
 
-        if (!(await comparePassword(password, queryResult.password))) {
-            console.log("Incorrect password!")
-            return res.json({success: false, token: null, msg: "Incorrect password!"})
+        const storedHashedPassword = queryResult[0].password;
+        const isPasswordMatch = await comparePassword(password, storedHashedPassword);
+
+        if (!isPasswordMatch) {
+            return res.status(401).json({ success: false, msg: "Incorrect password!" });    // unauthorized access
         }
 
-        const token = jwt.sign({username: username}, 'i-love-programming'/*, { expiresIn: 'never' }*/);
-        res.json({success: true, token: token, msg: "User is logged in!"})
+        // TODO: set up JWT and refresh token instead of session authentication
+        // const token = jwt.sign({username: username}, 'i-love-programming'/*, { expiresIn: 'never' }*/);
+
+        req.session.user = queryResult[0];
+        req.session.loggedIn = true;
+        console.log("User successfully logged in!");
+        return res.status(200).json({ success: true, msg: "User is logged in!" });  // all ok
     } catch (err) {
-        console.error(err)
+        console.error(err);
+        return res.status(500).json({ success: false, msg: "Internal server error!" });     // internal server error
     }
-
 });
+
+users.post('/logout', (req, res) => {
+    if (!(req.session.loggedIn)) {
+        return res.status(200).json({ success: true, msg: "No active session found." })
+    }
+    req.session.destroy(err => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ success: false, msg: "Failed to log out. Please try again." });
+        } else {
+            return res.status(200).json({ success: true, msg: "Successfully logged out." });
+        }
+    });
+})
+
+users.get('/session', async (req, res, next) => {
+    try {
+        res.json(req.session)
+    } catch (error) {
+        res.sendStatus(500)
+    }
+})
 
 users.post('/register', async (req, res, next) => {
     try {
         // TODO: if user is already logged it, new user cannot be added, but for that sessions are needed
 
-        const name = req.body.name
-        const surname = req.body.surname
-        const username = req.body.username
-        const email = req.body.email
+        const { name, surname, username, email, password, password2 } = req.body;
 
-        if (!(name && surname && username && email && req.body.password && req.body.password2)) {
-            return res.json({success: false, msg: "Input field missing! Please fill in all the fields."})
+        if (!(name && surname && username && email && password && password2)) {
+            return res.json({ success: false, msg: "Input field missing! Please fill in all the fields." });
         }
 
-        if (req.body.password !== req.body.password2) {
-            return res.json({success: false, msg: "Password does not match!"})
+        if (password !== password2) {
+            return res.json({ success: false, msg: "Passwords do not match!" });
         }
-        const password = await hashPassword(req.body.password)
-        if (!password) {
-            console.log("Error hashing password!")
-            return
+
+        const hashedPassword = await hashPassword(password);
+        if (!hashedPassword) {
+            console.log("Error hashing password!");
+            return res.status(500).json({ success: false, msg: "Internal server error!" });
         }
 
         // TODO: check if email is really an email, consists of username [at] provider [dot] domain
 
         // TODO: validate ALL user input before sending to DB to prevent SQL injection
 
-        const queryResultUsername = await DB.authUsername(username)
-        const queryResultEmail = await DB.authEmail(email)
+        const queryResultUsername = await DB.authUsername(username);
+        const queryResultEmail = await DB.authEmail(email);
 
         if (queryResultEmail.length > 0) {
-            return res.json({success: false, msg: "User with that E-mail already exists!"})
+            return res.json({ success: false, msg: "User with that E-mail already exists!" });
         }
 
         if (queryResultUsername.length > 0) {
-            return res.json({success: false, msg: "User with that Username already exists!"})
+            return res.json({ success: false, msg: "User with that Username already exists!" });
         }
 
-
-        const queryResult = await DB.addUser(name, surname, username, email, password);
+        const queryResult = await DB.addUser(name, surname, username, email, hashedPassword);
         if (!(queryResult.affectedRows)) {
-            res.json({success: false, msg: "Error registering new user..."})
+            return res.json({ success: false, msg: "Error registering new user..." });
         }
-        return res.json({success: true, msg: "New user successfully registered."})
+
+        return res.json({ success: true, msg: "New user successfully registered." });
     } catch (err) {
-        console.error(err)
+        console.error(err);
+        return res.status(500).json({ success: false, msg: "Internal server error!" });
     }
-})
+});
 
-// TODO: session function, to check if there exists a session
+// TODO: when registering, verify the user with email confirmation???
 
-// TODO: logout funcrion, to log out already logged in user
+// TODO: reset password function: reset the password for given user, overwrite password, send link to email, and when clicked, new password should be entered
+
+// Some way of logging, keeping logs of what is going on, like which user logs in registers logs out at what time etc.
 
 module.exports = users
