56bde898e8e9a96e4f075634719e013784cacb68
zstoimchev <zivkostoimcev1551@outlook.com>
2024-07-11 23:38:14 +0200

    created session for logging in and authentication

--- back-end/index.js
+++ back-end/index.js
@@ -1,33 +1,44 @@
 const express = require('express')
 const cors = require("cors")
-// const session = require('express-session')
+const session = require('express-session')
 // const cookieParser = require("cookie-parser");
 
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 8127
 
-
 // app.use(cookieParser());
 
-app.use(express.urlencoded({ extended: true }));
+let sess = {
+    secret: 'our litle secrett',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+    name: "app",
+    //     proxy: true,
 
+}
+
+// configurations
+app.use(express.json());
+app.use(session(sess))
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     methods: ["GET", "POST"],
     credentials: true,
     origin: ['http://localhost:3000', 'http://localhost:3001']
 }))
 
-app.get("/", (req, res) => {
-    res.send("hola")
-})
 
-const posts = require("./routes/posts")
 const users = require("./routes/users")
-app.use('/posts', posts)
+const posts = require("./routes/posts")
 app.use('/users', users)
+app.use('/posts', posts)
 
-///App listening on port
-app.listen(process.env.PORT || port, () => {
-    console.log(`Server is running on port: ${process.env.PORT || port}`)
-})
+// here most probably path needs to be initialized for the brontend build directory
+// and then app uses that build directory, and sends file index.html in the root page?
+
+
+app.get("/", (req, res) => { res.send("Tech Skill-Swap - HOME page. Welcome!") })
+
+app.listen(port, () => { console.log(`Server is running on port: ${port}`) })
