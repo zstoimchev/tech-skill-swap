b08ff7724d1b98416b9c5299f5b89d5174e4bcf5
zstoimchev <zivkostoimcev1551@outlook.com>
2024-07-14 11:11:13 +0200

    added basic routes for fetching all and one post(s)

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -2,10 +2,35 @@ const express = require("express")
 const posts = express.Router();
 const DB = require('../DB/dbConn.js')
 
-//Gets all the news in the DB 
-posts.post('/', async (req, res) => {
-    console.log("posts")
-    res.send()
+
+posts.get('/', async (req, res, next) => {
+    if (!(req.session.loggedIn)) {
+        return res.status(401).json({ success: false, msg: "Need to log in!" })
+    }
+    try {
+        const queryResult = await DB.allPosts()
+        return res.status(200).json(queryResult, { success: true, msg: "All posts fetches." })
+        res.json(queryResult)
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ success: false, msg: "Sometging happened!" })
+        res.sendStatus(500)
+        next()
+    }
 })
+
+posts.get('/:id', async (req, res, next) => {
+    try {
+        const queryResult = await DB.onePost(req.params.id)
+        res.json(queryResult)
+    }
+    catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+        next()
+    }
+})
+
 
 module.exports = posts
