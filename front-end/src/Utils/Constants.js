4bb53bc21873c5843cee774b74ea929a676c3d8b
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-07-14 22:27:34 +0200

    constant aded for one post

--- front-end/src/Utils/Constants.js
+++ front-end/src/Utils/Constants.js
@@ -1,6 +1,7 @@
 export const HOME = 'home';
 export const ABOUT = 'about';
 export const POSTS = 'posts';
+export const POST = 'post'
 export const REGISTER = 'register';
 export const LOGIN = 'login';
 export const LOGOUT = 'logout';
