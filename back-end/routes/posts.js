ca377de8cb6348c3a0d907fdfdd555ed81f35232
zstoimchev <zivkostoimcev1551@outlook.com>
2024-07-14 23:55:09 +0200

    succesfully fetching all and one posts respectfully

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -1,36 +1,53 @@
 const express = require("express")
-const posts = express.Router();
+const posts = express.Router()
 const DB = require('../DB/dbConn.js')
-
+const jwt = require('jsonwebtoken')
+const util = require('util')
+const jwtVerify = util.promisify(jwt.verify)
+const UTILS = require('../utils/functions.js')
 
 posts.get('/', async (req, res, next) => {
-    if (!(req.session.loggedIn)) {
-        return res.status(401).json({ success: false, msg: "Need to log in!" })
-    }
     try {
         const queryResult = await DB.allPosts()
-        return res.status(200).json(queryResult, { success: true, msg: "All posts fetches." })
-        res.json(queryResult)
+        return res.status(200).json({ arr: queryResult, success: true, msg: "All posts fetches." })
     }
     catch (err) {
         console.log(err)
         return res.status(500).json({ success: false, msg: "Sometging happened!" })
-        res.sendStatus(500)
-        next()
+        // next()
     }
 })
 
 posts.get('/:id', async (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) {
+        console.log("No token")
+        return res.status(401).json({ success: false, msg: "No JWT token found. Please login first." })
+    }
+
+    try {
+        const user = await jwtVerify(token, process.env.JWT_TOKEN_SECRET);
+        req.user = user;
+    } catch (err) {
+        console.log("Invalid token")
+        return res.status(403).json({ success: false, msg: "JWT token expired! Please log in again." })
+    }
+
+    if (!(UTILS.verifyId(req.params.id))) {
+        return res.status(200).json({ success: false, msg: "Bad ID! Try again." })
+    }
+
     try {
         const queryResult = await DB.onePost(req.params.id)
-        res.json(queryResult)
+        return res.status(200).json({ arr: queryResult[0], success: true, msg: "Post succesfully fetched." })
     }
     catch (err) {
         console.log(err)
-        res.sendStatus(500)
-        next()
+        return res.status(500).json({ success: false, msg: "omething happened. Internal server error" })
+        // next()
     }
 })
-
 
 module.exports = posts
