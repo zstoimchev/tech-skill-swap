acfc15550df90f3057dce1028878d2998f9fe47a
zstoimchev <zivkostoimcev1551@outlook.com>
2024-07-15 22:17:25 +0200

    fixing bug

--- back-end/routes/reset.js
+++ back-end/routes/reset.js
@@ -20,38 +20,47 @@ let transporter = nodemailer.createTransport({
 })
 
 reset.post('/reset', async (req, res) => {
-    // TODO: if the user is logged in, check existing password and prompt for new one
+    try {
+        // TODO: if the user is logged in, check existing password and prompt for new one
 
-    const email = req.body.email
+        const email = req.body.email
+        if (!email) {
+            return res.status(400).json({ success: false, msg: "Please fill in the field!" })
+        }
 
-    // TODO: check if the email is really an email & prevent SQL injection
+        if (!(UTILS.verifyEmail(email))) {
+            return res.status(400).json({ success: false, msg: "Invalid email!" })
+        }
 
-    const user = await DB.authEmail(email)
-    if (user.length != 1) {
-        return res.status(404).json({ success: false, msg: "No such user found!" })
-    }
+        const user = await DB.authEmail(email)
+        if (user.length != 1) {
+            return res.status(404).json({ success: false, msg: "No such user found!" })
+        }
 
-    const secretKey = process.env.JWT_SECRET
-    const expiresIn = '1h'
-    const token = jwt.sign({ email: email }, secretKey, { expiresIn })
+        const secretKey = process.env.JWT_SECRET
+        const expiresIn = '1h'
+        const token = jwt.sign({ email: email }, secretKey, { expiresIn })
 
-    let mailOptions = {
-        from: process.env.MY_EMAIL,
-        to: email,
-        subject: 'Password Reset - Tech Skill-Swap',
-        text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+        let mailOptions = {
+            from: process.env.MY_EMAIL,
+            to: email,
+            subject: 'Password Reset - Tech Skill-Swap',
+            text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
                Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n
                http://localhost:3000/reset/${token}\n\n
                If you did not request this, please ignore this email and your password will remain unchanged.\n`
-    }
-
-    transporter.sendMail(mailOptions, (err, response) => {
-        if (err) {
-            return res.status(500).json({ success: false, msg: "Error sending mail to the user." })    // internal server error
-        } else {
-            return res.status(200).json({ success: true, msg: "Recovery email sent!" })     // everything is ok
         }
-    })
+
+        transporter.sendMail(mailOptions, (err, response) => {
+            if (err) {
+                return res.status(500).json({ success: false, msg: "Error sending mail to the user." })    // internal server error
+            } else {
+                return res.status(200).json({ success: true, msg: "Recovery email sent!" })     // everything is ok
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({ success: false, msg: "Something happened. Please try again later." })
+    }
 })
 
 reset.get('/reset/:token', async (req, res) => {
