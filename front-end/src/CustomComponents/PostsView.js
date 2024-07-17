2f5ef54e15117cb25cef2841f7e4b7611358c649
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-07-17 18:56:32 +0200

    added author and date of post

--- front-end/src/CustomComponents/PostsView.js
+++ front-end/src/CustomComponents/PostsView.js
@@ -32,28 +32,30 @@ class PostsView extends React.Component {
 
     render() {
         const data = this.state.Posts
-        return (
+        return (<div>
+            <h1>Here goes some search bar. SEARCH BAR</h1>
             <div className="row row-cols-1 row-cols-md-3 g-4" style={{margin: "10px"}}>
-                {data.length > 0 ?
-                    data.map((d) => {
-                        return (
-                            <div className="col" key={d.id}>
-                                <div className="card">
-                                    <div className="card-body">
-                                        <h5 className="card-title">{d.title}</h5>
-                                        <p className="card-text">{d.body}</p>
-                                    </div>
-                                    <button onClick={() => this.props.changeState({CurrentPage: POST, id: d.id})}
-                                            style={{margin: "10px"}} className="btn btn-primary bt">Read more
-                                    </button>
-                                </div>
+                {data.length > 0 ? data.map((d) => {
+                    return (<div className="col" key={d.id}>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{d.title}</h5>
+                                <p style={{height: "5rem", overflow: "hidden"}}
+                                   className="card-text">{d.body}</p>
+                                <p className="card-text">
+                                    Author: {d.name} {d.surname}<br/>
+                                    Date: {d.created_at}
+                                </p>
                             </div>
-                        )
-                    })
-                    : "Loading..."}
-
+                            <button onClick={() => this.props.changeState({CurrentPage: POST, id: d.id})}
+                                    style={{margin: "10px"}} className="btn btn-primary bt">Read more
+                            </button>
+                        </div>
+                    </div>)
+                }) : "Loading..."}
             </div>
-        )
+
+        </div>)
     }
 
 }
