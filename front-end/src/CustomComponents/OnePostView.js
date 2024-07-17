d3cfac3a90bbec1ec592adedbf1be8dac7b48361
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-07-17 23:31:23 +0200

    added ability to comment on post, as well to see all other comments

--- front-end/src/CustomComponents/OnePostView.js
+++ front-end/src/CustomComponents/OnePostView.js
@@ -2,17 +2,23 @@ import React from "react"
 import PropTypes from 'prop-types'
 import axios from 'axios'
 import {API_URL} from "../Utils/Configuration"
-import {LOGIN, POSTS} from "../Utils/Constants"
+import {LOGIN, POST, POSTS} from "../Utils/Constants"
 
 class OnePostView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            post: {}
+            post: {}, comments: {}, user: {
+                comment: "", username: localStorage.getItem("user"),
+            }, status: {
+                success: null, msg: ""
+            },
+
         }
     }
 
     componentDidMount() {
+        this.setState({id: this.props.id})
         axios.get(API_URL + "/posts/" + this.props.id, {
             headers: {
                 'Authorization': `Bearer ${localStorage.getItem('token')}`
@@ -23,6 +29,7 @@ class OnePostView extends React.Component {
                     this.setState({
                         post: response.data.arr
                     })
+                    this.fetchComments()
                 } else {
                     console.log("No post data received");
                 }
@@ -31,11 +38,52 @@ class OnePostView extends React.Component {
                 console.error(error.response.data);
                 this.props.changeState({CurrentPage: LOGIN})
             })
+    }
 
+    Send() {
+        axios.post(API_URL + "/posts/comment", {
+            user: this.state.user.username, post_id: this.state.id, content: this.state.user.comment,
+        })
+            .then(response => {
+                this.setState({status: response.data})
+                console.log(response.data)
+                this.fetchComments()
+            })
+            .catch(error => {
+                console.error(error.response.data);
+                // this.props.changeState({CurrentPage: LOGIN})
+            })
+    }
+
+    fetchComments() {
+        axios.get(API_URL + "/posts/comment/" + this.state.id, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+            .then(response => {
+                if (response.data.success && response.data.arr) {
+                    this.setState({
+                        comments: response.data.arr
+                    })
+                } else {
+                    console.log("No comments data received");
+                }
+            })
+            .catch(error => {
+                console.error(error.response.data);
+            })
+    }
+
+    SetValueFromUserInput = (e) => {
+        this.setState(prevState => ({
+            user: {...prevState.user, [e.target.id]: e.target.value}
+        }))
     }
 
     render() {
         let post = this.state.post
+        const comments = this.state.comments
         return (<div className="card" style={{margin: "10px"}}>
             {Object.keys(post).length !== 0 ? <div>
                 <h5 className="card-header">{post.title}</h5>
@@ -46,7 +94,26 @@ class OnePostView extends React.Component {
                     <button onClick={() => this.props.changeState({CurrentPage: POSTS})}
                             className="btn btn-primary">Return news
                     </button>
+
                 </div>
+
+                <hr/>
+                <div className="card-body" style={{paddingTop: "0px"}}>
+                    <h5 style={{marginBottom: "10px"}}>Leave a comment:</h5>
+                    <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                        <input id="comment" type="text" className="form-control" placeholder="Enter your comment here"
+                               style={{marginRight: "10px", flex: "1"}} onChange={this.SetValueFromUserInput}/>
+                        <button className="btn btn-primary" onClick={() => this.Send()}>Post</button>
+                    </div>
+                </div>
+                <hr/>
+                <div className="card-body" style={{paddingTop: "0px"}}>
+                    <h5>Other comments:</h5>
+                    {comments.length > 0 ? comments.map((d) => {
+                        return (<p key={d.id}><b>{d.username}</b>: {d.content} - <i>{d.date}</i></p>)
+                    }) : "There are no comments yet..."}
+                </div>
+
             </div> : "Loading..."}
         </div>)
     }
