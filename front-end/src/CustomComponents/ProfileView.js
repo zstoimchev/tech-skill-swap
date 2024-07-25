4eae78dc74a8308ba2a76d1b98e14f3cbca14842
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-07-26 01:23:40 +0200

    added visual buttons to change name/email

--- front-end/src/CustomComponents/ProfileView.js
+++ front-end/src/CustomComponents/ProfileView.js
@@ -7,8 +7,22 @@ class ProfileView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            Posts: [], User: null, username: localStorage.getItem("user"),
+            Posts: [],
+            User: null,
+            username: localStorage.getItem("user"),
+            editName: false,
+            editEmail: false,
+            editUsername: false,
+            userInput: {
+                name: "", surname: "", email: "", username: "", role: "", password: "", password2: "",
+            },
         }
+    }
+
+    SetValueFromUserInput = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
     }
 
     componentDidMount() {
@@ -22,6 +36,10 @@ class ProfileView extends React.Component {
             })
     }
 
+    editNameSurname = () => {
+        // TODO: call the API and save the new name and surname
+        this.setState({editName: !this.state.editName});
+    }
 
     render() {
         const p = this.state.Posts
@@ -32,31 +50,105 @@ class ProfileView extends React.Component {
         return (<div className="card" style={{margin: "10px"}}>
             <div className="card-body">
 
+                {/*CHANGE NAME/SURNAME*/}
                 <div className={"d-flex align-items-center"}>
-                    <h2 className="card-title"><b>{this.state.User.name} {this.state.User.surname}</b></h2>
-                    <button className={"btn btn-primary btn-md ms-auto"}>Edit Name/Surname</button>
+                    {!this.state.editName ? (<>
+                        <h1 className="card-title"><b>{this.state.User.name} {this.state.User.surname}</b></h1>
+                        <button onClick={() => this.setState({editName: !this.state.editName})}
+                                className={"btn btn-primary btn-md ms-auto"}>Edit
+                            Name/Surname
+                        </button>
+                    </>) : (<>
+                        <div className="row mb-3">
+                            <div className="col">
+                                <input onChange={this.SetValueFromUserInput}
+                                       type="text"
+                                       className="form-control"
+                                       id="name"
+                                       defaultValue={this.state.User.name}/>
+                            </div>
+
+                            <div className="col">
+                                <input onChange={this.SetValueFromUserInput}
+                                       type="text"
+                                       className="form-control"
+                                       id="surname"
+                                       defaultValue={this.state.User.surname}/>
+                            </div>
+                        </div>
+                        <div className="ms-auto">
+                            <button onClick={this.editNameSurname}
+                                    className={"btn btn-success btn-md me-1"}>Submit
+                            </button>
+                            <button onClick={() => this.setState({editName: false})}
+                                    className={"btn btn-danger btn-md ms-auto"}>Cancel
+                            </button>
+                        </div>
+                    </>)}
                 </div>
                 <hr/>
 
+                {/*CHANGE EMAIL*/}
                 <div className={"d-flex align-items-center"}>
-                    <h5>Email: <i>{this.state.User.email}</i></h5>
-                    <button className={"btn btn-primary btn-sm ms-auto"}>Update E-mail</button>
+                    {!this.state.editEmail ? (<> <h5 className="card-title">E-mail: <b>{this.state.User.email}</b></h5>
+                        <button onClick={() => this.setState({editEmail: !this.state.editEmail})}
+                                className={"btn btn-primary btn-sm ms-auto"}>Update E-mail
+                        </button>
+                    </>) : (<>
+                        <div className="row mb-0">
+                            <input onChange={this.SetValueFromUserInput}
+                                   type="text"
+                                   className="form-control"
+                                   id="email"
+                                   defaultValue={this.state.User.email}/>
+                        </div>
+                        <div className="ms-auto">
+                            <button onClick={this.editEmail}
+                                    className={"btn btn-success btn-sm me-1"}>Submit
+                            </button>
+                            <button onClick={() => this.setState({editEmail: false})}
+                                    className={"btn btn-danger btn-sm ms-auto"}>Cancel
+                            </button>
+                        </div>
+                    </>)}
                 </div>
 
+                {/*CHANGE USERNAME*/}
                 <div className={"d-flex align-items-center"}>
-                    <h5>Username: <i>{this.state.User.username}</i></h5>
-                    <button className={"btn btn-primary btn-sm ms-auto"}>Edit username</button>
+                    {!this.state.editUsername ? (<>
+                        <h5 className="card-title">Username: <b>{this.state.User.username}</b></h5>
+                        <button onClick={() => this.setState({editUsername: !this.state.editUsername})}
+                                className={"btn btn-primary btn-sm ms-auto"}>Edit username
+                        </button>
+                    </>) : (<>
+                        <div className="row mb-0">
+                            <div className="col">
+                                <input onChange={this.SetValueFromUserInput}
+                                       type="text"
+                                       className="form-control"
+                                       id="username"
+                                       defaultValue={this.state.User.username}/>
+                            </div>
+                        </div>
+                        <div className="ms-auto">
+                            <button onClick={this.editUsername}
+                                    className={"btn btn-success btn-sm me-1"}>Submit
+                            </button>
+                            <button onClick={() => this.setState({editUsername: false})}
+                                    className={"btn btn-danger btn-sm ms-auto"}>Cancel
+                            </button>
+                        </div>
+                    </>)}
                 </div>
 
-                <div className={"d-flex align-items-center"}>
-                    <h5>Role: <i>Seeker or Helper</i></h5>
-                </div>
-
+                {/*CHANGE PASSWORD*/}
                 <h5>Change your password, click here</h5>
                 <hr/>
 
+                {/*ALL POSTS AUTHORED*/}
                 <div className="row row-cols-1 g-4" style={{margin: "10px"}}>
-                    <h4 className={"card-title"} style={{fontWeight:"bold"}}>All posts asking/requesting for help:</h4>    {p.length > 0 ? p.map((d) => {
+                    <h4 className={"card-title"} style={{fontWeight: "bold"}}>All posts asking/requesting for
+                        help:</h4>    {p.length > 0 ? p.map((d) => {
                     return (<div className="col" key={d.id}>
                         <div className="card">
                             <div className="card-body">
