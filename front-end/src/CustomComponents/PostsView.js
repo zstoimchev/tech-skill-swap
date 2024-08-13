6b9f6d3006b74bb6054a3c2a22194ac4f5977bc0
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-08-14 00:04:53 +0200

    added search post by category

--- front-end/src/CustomComponents/PostsView.js
+++ front-end/src/CustomComponents/PostsView.js
@@ -2,12 +2,13 @@ import React from "react";
 import axios from "axios";
 import {API_URL} from "../Utils/Configuration";
 import {POST} from "../Utils/Constants";
+import Dropdown from 'react-bootstrap/Dropdown';
 
 class PostsView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            Posts: [], payload: "",
+            Posts: [], payload: "", cat: []
         }
         this.req = axios.create({
             withCredentials: true, baseURL: API_URL, headers: {
@@ -18,14 +19,15 @@ class PostsView extends React.Component {
 
     componentDidMount() {
         this.fetchAllPosts()
+        this.fetchCategories()
     }
 
     fetchAllPosts = () => {
         axios.get(API_URL + '/posts')
             .then(response => {
-                if (Array.isArray(response.data.arr)) {
+                if (Array.isArray(response.data["arr"])) {
                     this.setState({
-                        Posts: response.data.arr
+                        Posts: response.data["arr"]
                     })
                 } else {
                     console.error(response.data.msg);
@@ -38,8 +40,25 @@ class PostsView extends React.Component {
             })
     }
 
+    fetchCategories = () => {
+        axios.get(API_URL + '/posts/category/get')
+            .then(response => {
+                if (Array.isArray(response.data["arr"])) {
+                    this.setState({
+                        cat: response.data["arr"]
+                    })
+                } else {
+                    console.error(response.data.msg);
+                }
+            })
+            .catch(error => {
+                console.error(error.response.data)
+                this.setState({status: error.response.data})
+            })
+    }
+
     SetValueFromUserInput = (event) => {
-        this.setState({ [event.target.id]: event.target.value }, () => {
+        this.setState({[event.target.id]: event.target.value}, () => {
             if (this.state.payload === '') {
                 this.fetchAllPosts();
             } else {
@@ -51,9 +70,9 @@ class PostsView extends React.Component {
     PerformSearchInDb = () => {
         this.req.get('/posts/search/' + this.state.payload)
             .then(response => {
-                if (Array.isArray(response.data.posts)) {
+                if (Array.isArray(response.data["posts"])) {
                     this.setState({
-                        Posts: response.data.posts
+                        Posts: response.data["posts"]
                     })
                 } else {
                     console.error(response.data.msg)
@@ -65,40 +84,70 @@ class PostsView extends React.Component {
             })
     }
 
+    searchCategory = (id) => {
+        axios.get(API_URL + '/posts/category/get/' + id)
+            .then(response => {
+                console.log("---")
+                console.log(response.data)
+                if (Array.isArray(response.data["arr"])) {
+                    this.setState({
+                        Posts: response.data["arr"]
+                    })
+                } else {
+                    console.error(response.data.msg);
+                }
+            })
+            .catch(error => {
+                console.error(error.response.data)
+                this.setState({status: error.response.data})
+            })
+    }
+
 
     render() {
         const data = this.state.Posts
         return (<div>
 
             <div className="row row-cols-1 row-cols-md-3 g-4" style={{margin: "10px"}}>
-                {/*<h3 className="input-group">Browse the content free of charge</h3>*/}
                 <div className="w-100 d-flex justify-content-center">
                     <h3>Browse the content free of charge</h3>
                 </div>
 
                 <div className="input-group">
-                    <div className="input-group-append">
-                        <select onChange={this.SetValueFromUserInput} className="form-control w-100" id="role">
-                            <option value="">Toggle Category</option>
-                            <option value="Helper">Helper, I want to help people</option>
-                            <option value="Seeker">Seeker, I want to ask for help</option>
-                            <option value="both">Both helping and requesting help...</option>
-                        </select>
-                    </div>
+                    <Dropdown>
+                        <Dropdown.Toggle id="dropdown-basic">
+                            Search by Category
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={() => this.fetchAllPosts()}>Get all posts</Dropdown.Item>
+                            {this.state.cat.length > 0 ? this.state.cat.map((t) => (
+                                <Dropdown.Item key={t.id} onClick={() => this.searchCategory(t.id)}>
+                                    {t.name}
+                                </Dropdown.Item>)) : null}
+                        </Dropdown.Menu>
+                    </Dropdown>
+
                     <input id="payload" type="search" className="form-control rounded" placeholder="Search"
-                           aria-label="Search"
+                           aria-label="Search" value={this.state.payload}
                            aria-describedby="search-addon" onChange={this.SetValueFromUserInput}/>
 
-
-                    <button onClick={this.PerformSearchInDb} type="button" className="btn btn-outline-primary"
+                    <button onClick={this.PerformSearchInDb} type="button" className="btn btn-outline-success"
                             data-mdb-ripple-init="">search
+                    </button>
+                    <button type="button" className="btn btn-outline-danger"
+                            onClick={() => {
+                                this.fetchAllPosts()
+                                this.setState({payload: ""})
+                            }}
+                            data-mdb-ripple-init="">Remove all filters
                     </button>
                 </div>
 
 
                 {data.length > 0 ? data.map((d) => {
                     const date = new Date(d["created_at"])
-                    const options = { year: 'numeric', month: '2-digit', day: '2-digit' }
+                    const options = {year: 'numeric', month: '2-digit', day: '2-digit'}
                     const formattedDate = date.toLocaleDateString('en-GB', options) + ' @ ' + date.toLocaleTimeString()
                     return (<div className="col" key={d.id}>
                         <div className="card">
