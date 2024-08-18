7d6051e874a75f43dca5933651f767c88de07597
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-08-18 03:49:06 +0200

    showing comments in author view too

--- front-end/src/CustomComponents/AuthorView.js
+++ front-end/src/CustomComponents/AuthorView.js
@@ -12,7 +12,7 @@ class AuthorView extends React.Component {
                 name: "", surname: "", email: "", username: "", role: "",
             }, username: this.props.username, status: {
                 success: null, msg: ""
-            }, role: "Helper Seeker",
+            }, role: "Helper Seeker", showComments: false, comments: [],
         }
         this.req = axios.create({
             withCredentials: true, baseURL: API_URL, headers: {
@@ -25,7 +25,15 @@ class AuthorView extends React.Component {
         console.log(this.props.username)
         this.setState({username: this.props.username})
         await this.getUserData()
+    }
 
+    getCommentsFromUser = () => {
+        this.req.get('profile/comments/' + this.state.User.username).then(response => {
+            this.setState({comments: response.data.comments})
+        }).catch(error => {
+            this.setState({status: error.response.data})
+            console.error(error.response.data)
+        })
     }
 
     getUserData() {
@@ -33,6 +41,8 @@ class AuthorView extends React.Component {
             .then(response => {
                 this.setState({
                     Posts: response.data["postData"], User: response.data["userData"], role: response.data["roleData"]
+                }, () => {
+                    this.getCommentsFromUser();
                 })
             })
             .catch(error => {
@@ -43,8 +53,10 @@ class AuthorView extends React.Component {
 
     sortByDate = () => {
         this.setState(prevState => ({
-            Posts: [...prevState.Posts].reverse(), isAscending: !this.state.isAscending
-        }));
+            Posts: [...prevState.Posts].reverse(),
+            comments: [...prevState.comments].reverse(),
+            isAscending: !this.state.isAscending
+        }))
     }
 
     render() {
@@ -59,8 +71,8 @@ class AuthorView extends React.Component {
                 <div className={"d-flex align-items-center"}>
                     <h1 className="card-title"><b>{this.state.User.name} {this.state.User.surname}</b></h1>
                     <button
-                            className={"btn btn-outline-primary btn-lg ms-auto"}
-                            onClick={() => window.location = `mailto:${this.state.User.email}`}>Contact
+                        className={"btn btn-outline-primary btn-lg ms-auto"}
+                        onClick={() => window.location = `mailto:${this.state.User.email}`}>Contact
                     </button>
                 </div>
 
@@ -133,34 +145,63 @@ class AuthorView extends React.Component {
                 {/*ALL POSTS AUTHORED*/}
                 <div className="row row-cols-1 g-4" style={{margin: "10px"}}>
                     <div className="d-flex justify-content-between align-items-center">
-                        <h4 className={"card-title"} style={{fontWeight: "bold"}}>All posts asking/requesting for
-                            help:</h4>
-                        <button
-                            onClick={this.sortByDate}
-                            className="btn btn-outline-primary"
-                            style={{marginLeft: "auto"}}
-                        >
-                            Sort by Date
-                            <span className="badge badge-dark">
+                        {!this.state.showComments ?
+                            <h4 className={"card-title"} style={{fontWeight: "bold"}}>All posts asking/requesting for
+                                help:</h4> :
+                            <h4 className={"card-title"} style={{fontWeight: "bold"}}>All comments even recorded</h4>}
+                        <div>
+                            <button
+                                className={"btn btn-outline-secondary btn-md ms-auto me-1"}
+                                onClick={() => this.setState({showComments: !this.state.showComments})}>
+                                {!this.state.showComments ? "View Comments" : "View Posts"}
+                            </button>
+                            <button
+                                onClick={this.sortByDate}
+                                className="btn btn-outline-primary"
+                                style={{marginLeft: "auto"}}
+                            >
+                                Sort by Date
+                                <span className="badge badge-dark">
                                 {this.state.isAscending ? (<i className="bi bi-arrow-up text-primary"></i>) : (
                                     <i className="bi bi-arrow-down text-primary"></i>)}
                             </span>
-                        </button>
-
+                            </button>
+                        </div>
                     </div>
-                    {p.length > 0 ? p.map((d) => {
-                        return <div className="col" key={d.id}>
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">{d.title}</h5>
-                                    <p className="card-text">{d.body}</p>
-                                    <button onClick={() => this.props.changeState({CurrentPage: POST, id: d.id})}
-                                            style={{margin: "10px"}} className="btn btn-primary bt">Read more
-                                    </button>
+                    {!this.state.showComments ? <>
+                        {p.length > 0 ? p.map((d) => {
+                            return <div className="col" key={d.id}>
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{d.title}</h5>
+                                        <p className="card-text">{d.body}</p>
+                                        <button onClick={() => this.props.changeState({CurrentPage: POST, id: d.id})}
+                                                style={{margin: "10px"}} className="btn btn-primary bt">Read more
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    }) : "No help requested yet"}
+                        }) : "No help requested yet"}
+                    </> : null}
+                    {this.state.showComments ? <>
+                        {this.state.comments.length > 0 ? this.state.comments.map((d) => {
+                            const date = new Date(d.date)
+                            const options = {year: 'numeric', month: '2-digit', day: '2-digit'}
+                            const formattedDate = date.toLocaleDateString('en-GB', options) + ' @ ' + date.toLocaleTimeString()
+                            return <div className="col mb-0" key={d.id}>
+                                <div className="card-body p-0">
+                                    <div className="d-flex justify-content-between align-items-center mb-0">
+                                        <h5 className="mb-0">Post Title: {d.title} - {formattedDate}</h5>
+                                        <button onClick={() => this.props.changeState({CurrentPage: POST, id: d.id})}
+                                                style={{margin: "10px"}} className="btn btn-md btn-primary mb-0">Visit
+                                            Post
+                                        </button>
+                                    </div>
+                                    <p className="mt-0 mb-0">Content: {d.content}</p>
+                                </div>
+                            </div>
+                        }) : "No comments found yet"}</> : null}
+
                 </div>
 
             </div>
