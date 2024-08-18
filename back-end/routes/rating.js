212165ba41e21c427f4daa704996985218e71dca
zstoimchev <zivkostoimcev1551@outlook.com>
2024-08-19 00:50:47 +0200

    added code for sending mail to author when someone rates, but need to fetch the email first

--- back-end/routes/rating.js
+++ back-end/routes/rating.js
@@ -2,6 +2,8 @@ const express = require("express")
 const rating = express.Router()
 const DB = require('../DB/dbConn.js')
 const UTILS = require('../utils/functions.js')
+const nodemailer = require('nodemailer')
+
 
 rating.get('/:post_id', async (req, res) => {
     try {
@@ -40,6 +42,19 @@ rating.get('/fetch-user/:post_id/:user', async (req, res) => {
     }
 })
 
+let transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: process.env.MY_EMAIL,
+        pass: process.env.MY_PASSWORD
+    },
+    tls: {
+        ciphers: 'SSLv3'
+    }
+})
+
 rating.post('/add', async (req, res) => {
     try {
         const { user, post_id, value } = req.body
@@ -64,11 +79,40 @@ rating.post('/add', async (req, res) => {
             }
             if (!queryRes.affectedRows)
                 return res.status(404).json({ success: false, msg: "Post not found..." })
-            return res.status(200).json({ success: true, msg: "Rating saved!" })
         } catch (error) {
             console.error(error)
             return res.status(503).json({ success: false, msg: "Error while processing DB for user comment" })
-        }        
+        }
+
+        // TODO: send mail to author that someone rated
+        // find the person, the author email and send the email
+
+        // let mailOptions = {
+        //     from: process.env.MY_EMAIL,
+        //     to: email,
+        //     subject: 'New Rating - Tech Skill-Swap',
+        //     html: `
+        //         <p>You are receiving this because you (or someone else) have submitted 
+        //         a rating for one of your posts.</p>
+                
+        //         <p>Please click on the following link, or paste this into your browser to review
+        //         the latest interactions on your posts:</p>
+                
+        //         <a href="http://88.200.63.148:8127/">Visit Account</a>
+
+        //         <p>If you did not request this, please ignore this email, hence no changes will be made.</p>
+        //         `
+        // }
+
+        // transporter.sendMail(mailOptions, (err, response) => {
+        //     if (err) {
+        //         return res.status(500).json({ success: false, msg: "Error sending mail to the user." })
+        //     } else {
+        //         return res.status(200).json({ success: true, msg: "Rating saved!" })
+        //     }
+        // })
+
+        return res.status(200).json({ success: true, msg: "Rating saved!" })
     } catch (error) {
         console.error(error)
         return res.status(500).json({ success: false, msg: "Internal server error..." })
